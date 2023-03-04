a593f555bff8a0d341ba3705cc2ea74c2bf44555
DirajThajali <thajalidiraj@gmail.com>
2023-03-04 14:01:37 -0800

    init + cleanup

---
+++ src/components/path-finder/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Node from '../node';
+
+export default class PathFinder extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+  render() {
+    return (
+      <>
+        <Node />
+      </>
+    )
+  }
+}
